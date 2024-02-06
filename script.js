705ca27dd1d6da5522a4a5029f9ebb8e014ab65b
Brandon Pouliot <brandonpouliot@gmail.com>
2024-02-05 19:45:52 -0500

    added api key and api url

--- script.js
+++ script.js
@@ -5,3 +5,7 @@ function searchMovie() {
     return;
 }
 
+const apikey = 'http://www.omdbapi.com/?i=tt3896198&apikey=23774e88';
+
+const apiUrl = `http://www.omdbapi.com/?t=${encodeURIComponent(movieTitle)}&apikey=${apiKey}`;
+

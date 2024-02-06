171ff34173e09bbbc8d990a6fa03924b605ffb9e
Brandon Pouliot <brandonpouliot@gmail.com>
2024-02-05 19:44:28 -0500

    added search movie funtion

---
+++ script.js
@@ -0,0 +1,7 @@
+function searchMovie() {
+    const movieTitle = document.getElementById('movieTitle').value;
+    if(movieTitle.trim() === "")
+    alert("Please enter movie title");
+    return;
+}
+

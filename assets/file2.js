dd56fc592ce9bc25701365d6b341a2e6d03b24b1
Brandon Pouliot <brandonpouliot@gmail.com>
2024-02-08 18:45:40 -0500

    removed unused and unneeded code

--- assets/file2.js
+++ assets/file2.js
@@ -1,4 +1,4 @@
-const apikey = '23774e88';
+const apiKey = '23774e88';
 
 function searchMovie() {
     const movieTitle = document.getElementById('movieTitle').value.trim();
@@ -33,18 +33,7 @@ function displaySearchResults(results) {
 }
 function handleSearch(event) {
     event.preventDefault();
-    const query = document.getElementById('searchInput').value.trim();
-    if (query === ''){
-        return;
-    }
-    searchMovie(query)
-    .then(results => {
-        if (results){
-            displaySearchResults(results);
-        } else {
-
-        }
-    });
+    searchMovie();
 }
 
 function saveSearchResultstoLocalStorage(query, results) {
@@ -60,29 +49,4 @@ function init() {
     const searchForm = document.getElementById('searchForm');
     searchForm.addEventListener('submit', handleSearch);
 }
-window.addEventListener('load', init);function searchMovie() {
-    const movieTitle = document.getElementById('movieTitle').value;
-    
-    if(movieTitle.trim() === "") {
-    alert("Please enter movie title");
-    return;
-    }
-
-    const apikey = 'http://www.omdbapi.com/?i=tt3896198&apikey=23774e88';
-
-    const apiUrl = `http://www.omdbapi.com/?t=${encodeURIComponent(movieTitle)}&apikey=${apiKey}`;
-
-    fetch(apiUrl)
-        .then(response => response.json())
-        .then(data => {
-            const movieDetailsContainer = document.getElementById('movieDetails');
-            if(data.Response === "True") {
-                movieDetailsContainer.innerHTML = 
-                <h2>${data.Title}</h2>;
-                <><p><strong>Year:</strong> ${data.Year}</p><p><strong>Genre:</strong> ${data.Genre}</p><p><strong>Plot:</strong> ${data.Plot}</p><p><strong>IMDb Rating:</strong> ${data.imdbRating}</p></>;
-            } else {
-                movieDetailsContainer.innerHTML = '<p>${data.Error}</p>';
-            }
-        })
-        .catch(error => console.error('Error fetching data:', error));
-} 
+window.addEventListener('load', init);
